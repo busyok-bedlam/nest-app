9b647fb99b3f8511f4395afba2d315ccf6f1c0d6
User <user@Users-MacBook-Pro.local>
2019-11-27 11:43:37 +0200

    finish websocket settings

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -4,19 +4,26 @@ import {
   OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  MessageBody
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+  WsResponse
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway(3001)
+@WebSocketGateway(3001, { namespace: 'chat' })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server
+
   private logger: Logger = new Logger('AppGateway');
   afterInit(server: Server) {
     this.logger.log('Initialized');
   }
   handleConnection(client: Socket) {
     this.logger.log(`Client connected ${client.id}`);
+    client.join('GENERAL_ROOM');
   }
 
   handleDisconnect(client: Socket) {
@@ -25,9 +32,10 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   }
 
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() payload: any): string {
-    console.log('RECEIVE MESSAGE FROM CLIENT');
-    console.log('PAYLOAD', payload)
-    return 'Hello world!';
+  handleMessage(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+    console.log('RECEIVE MESSAGE FRапаOM CLIENT');
+    console.log('PAYLOAD', data);
+    client.broadcast.emit('msgToClient', data);
+    // return { event: 'msgToClient', data}
   }
 }
--- static/main.js
+++ static/main.js
@@ -1,14 +1,19 @@
-(() => {
-    console.log(io)
-    const socket = io('http://localhost:3001');
+(($) => {
+    console.log($)
+    const socket = io('http://localhost:3001/chat');
     socket.on('connect', () => {
         console.log('CONNECT ON CLIENT ',socket.id); 
     });
 
-
+    socket.on('msgToClient', (message) => {
+        $('#message-block').append($('<div></div>').text(message));
+        $('#input').val('');
+    })
+    const input = document.getElementById("input");
     const button = document.getElementById("message-button");
     button.addEventListener("click", (e) => {
+        let value = input.value.trim() || null;
         e.preventDefault();
-        socket.emit('message', "DATA")
+        socket.emit('message', value)
     });
-})()
+})(jQuery)
