8a093043d707abca4e6b9323faca2899baea122c
BULAT_CHERKASHIN <busyokbedlam@gmail.com>
2019-11-29 15:28:51 +0200

    ADDAUTH

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, UsePipes, ForbiddenException, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, ForbiddenException, Param, Query, UseGuards } from '@nestjs/common';
+import { CatsGuard } from './cats.guard';
 
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
+@UseGuards(CatsGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
@@ -14,6 +16,11 @@ export class CatsController {
     this.catsService.create(createCatDto);
   }
 
+	@Get()
+	async find() {
+	
+    return this.catsService.findAll();
+	}
   @Get('/:id')
   async findAll(@Query() queryData, @Param() paramData ): Promise<Cat[]> {
     console.log("PARAMDATA",paramData);
