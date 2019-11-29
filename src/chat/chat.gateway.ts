50ead805038205a55a320272b5a1350bae6d478d
BULAT_CHERKASHIN <busyokbedlam@gmail.com>
2019-11-29 15:19:37 +0200

    add auth option

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -43,8 +43,8 @@ export class ChatGateway
     @ConnectedSocket() client: Socket,
   ) {
     client.broadcast.emit('msgToClient', data);
-    // throw new WsException('Invalid credentials.');
 
+		// throw new WsException('Invalid credentials.');
     return { event: 'msgToClient', data };
   }
 
