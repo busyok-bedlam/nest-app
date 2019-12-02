0e4891a0b06375427f4e502b41f55a6bfeb48a6a
BULAT_CHERKASHIN <busyokbedlam@gmail.com>
2019-12-02 17:33:21 +0200

    ADD EXCEPTION FILTER

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,35 +1,36 @@
-import { Controller, Get, Post, Body, UsePipes, ForbiddenException, Param, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, Param, Query, UseGuards, HttpStatus, HttpException,UseFilters} from '@nestjs/common';
 import { CatsGuard } from './cats.guard';
-
+import { ForbiddenException } from '../common/exeption/forbidden.exception'
+import { HttpExceptionFilter } from '../common/filters/http_exeption.filter';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
+@UseFilters(new HttpExceptionFilter())
 @UseGuards(CatsGuard)
 export class CatsController {
-  constructor(private readonly catsService: CatsService) {}
+	constructor(private readonly catsService: CatsService) {}
 
-  @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
-    console.log(createCatDto);
-    this.catsService.create(createCatDto);
-  }
+	@Post()
+	async create(@Body() createCatDto: CreateCatDto) {
+		console.log(createCatDto);
+		this.catsService.create(createCatDto);
+	}
 
 	@Get()
-	async find() {
-	
-    return this.catsService.findAll();
+	async find(): Promise<String> {
+		return "FJDKGKDKGJD";
 	}
-  @Get('/:id')
-  async findAll(@Query() queryData, @Param() paramData ): Promise<Cat[]> {
-    console.log("PARAMDATA",paramData);
-    console.log("QUERYDATA",queryData)
+	@Get('/:id')
+	async findAll(@Query() queryData, @Param() paramData ): Promise<Cat[]> {
+		console.log("PARAMDATA",paramData);
+		console.log("QUERYDATA",queryData)
 
-    return this.catsService.findAll();
-  }
-  @Get('/xxx')
-   xxx(): string {
-    return 'string';
-  }
+		return this.catsService.findAll();
+	}
+	@Get('/xxx')
+	xxx(): string {
+		return 'string';
+	}
 }
---
+++ src/common/exeption/forbidden.exception.ts
@@ -0,0 +1,10 @@
+import { HttpStatus, HttpException } from '@nestjs/common';
+
+
+
+
+export class ForbiddenException extends HttpException {
+	constructor() {
+		    super('Forbidden', HttpStatus.BAD_GATEWAY);			  
+	}
+}
